0d8867c503f57def0b3d11ef0415053bc65d8bd6
gchriswill <4576678+gchriswill@users.noreply.github.com>
2023-02-27 03:08:27 -0600

    inventory refactors and reorganization

--- src/Inventory/ShiaInventory.js
+++ src/Inventory/ShiaInventory.js
@@ -1,10 +1,26 @@
 // import Table from 'react-bootstrap/Table';
 // import { ShiaDefaultData } from "./../ShiaDefaultData.json"
 import { ShiaInventoryDataParser } from "./ShiaInventoryDataParser";
+import Table from "react-bootstrap/Table";
+import React from "react";
 
 export function ShiaInventory() {
 
   return (
-    <ShiaInventoryDataParser/>
+    <Table striped bordered hover variant="dark" style={{ width: "65%", marginLeft: "250px"}}>
+      <thead>
+      <tr>
+        <th>id</th>
+        <th>Name</th>
+        <th>Category</th>
+        <th>Price</th>
+        <th>Quantity</th>
+        <th>Threshold</th>
+      </tr>
+      </thead>
+      <tbody>
+      <ShiaInventoryDataParser/>
+      </tbody>
+    </Table>
   );
 }
