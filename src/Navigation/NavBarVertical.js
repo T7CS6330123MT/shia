aefc57b36c384bdfd9a5d0ab4f2bb8e637282a63
gchriswill <4576678+gchriswill@users.noreply.github.com>
2023-02-27 06:26:27 -0600

    Updates navigation
    Replaces navigation `Nav.Link` to `NavLink`, and points each link to their respective pages.

--- src/Navigation/NavBarVertical.js
+++ src/Navigation/NavBarVertical.js
@@ -2,15 +2,17 @@ import Nav from 'react-bootstrap/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./NavBarVertical.css";
 import { NavBarVerticalLogo } from "./NavBarVerticalLogo";
+import { NavLink } from "react-router-dom";
 
 export function NavBarVertical() {
   return (
   <div className="container-fluid row ShiaNavBarVerticalContainer" >
       <div className="col-2">
         <NavBarVerticalLogo/>
-        <Nav defaultActiveKey="/home" className="flex-column ShiaNavBar">
-          <Nav.Link className="ShiaNavBarVerticalLinks" href="/home">Inventory</Nav.Link>
-          <Nav.Link className="ShiaNavBarVerticalLinks" href="/notifications" eventKey="notifications">Notifications</Nav.Link>
+        <Nav defaultActiveKey="/dashboard" className="flex-column ShiaNavBar">
+          <NavLink className="ShiaNavBarVerticalLinks" to={"/dashboard"}>Dashboard</NavLink>
+          <NavLink className="ShiaNavBarVerticalLinks" to={"/inventory"}>Inventory</NavLink>
+          <NavLink className="ShiaNavBarVerticalLinks" to={"/notifications"}>Notifications</NavLink>
         </Nav>
       </div>
     </div>
