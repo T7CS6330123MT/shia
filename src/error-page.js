a7cb14ffd4f564ed92a1745a6c8f773e6ee52cfe
gchriswill <4576678+gchriswill@users.noreply.github.com>
2023-02-28 06:21:53 -0600

    Refactors error handling

--- src/error-page.js
+++ src/error-page.js
@@ -3,9 +3,10 @@ import { useRouteError } from "react-router-dom";
 export default function ErrorPage() {
 
   const error = useRouteError();
-  const errorData = error.data
-    .toString()
-    .replace("Error: ", "")
+  const status = error["status"] || ""
+  const statusText = error["statusText"] || ""
+  const internal = error["internal"] || ""
+  const errorData = error["data"] || ""
 
   console.error(error);
 
@@ -20,10 +21,12 @@ export default function ErrorPage() {
       <h1>Shia Has Encountered an Error!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i> <b>Error Code:</b> {error.status}</i><br/>
-        <i> <b>Error Description:</b> {error.statusText}</i><br/>
-        <i> <b>Error Internal:</b> {error.internal.toString()}</i><br/>
-        <i> <b>Error Details:</b> {errorData}</i>
+        <i> <b>Error Code:</b> {status}</i><br/>
+        <i> <b>Error Description:</b> {statusText}</i><br/>
+        <i> <b>Error Internal:</b> {internal.toString()}</i><br/>
+        <i> <b>Error Details:</b> {
+            errorData.toString().replace("Error: ", "")
+        }</i>
       </p>
     </div>
   );
