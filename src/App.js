c0ab5e08cfa110009204cd7d828d6e94d265d170
gchriswill <4576678+gchriswill@users.noreply.github.com>
2023-02-28 06:52:04 -0600

    Updates App.js with Authentication placeholder

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBarVertical } from "./Navigation/NavBarVertical.js";
 import { NavBarHorizontal } from "./Navigation/NavBarHorizontal.js";
 import { Outlet } from "react-router-dom";
+// import Authentication from "./Authentication/Authentication.js";
 
 export default function App() {
   return (
     <div className="container-fluid App bg-dark">
-      <NavBarHorizontal/>
-      <NavBarVertical/>
+      {/*<Authentication />*/}
+      <NavBarHorizontal />
+      <NavBarVertical />
       <Outlet />
     </div>
   );
