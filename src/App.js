a89993b632454edbfe1030c98345c89e0108b394
gchriswill <4576678+gchriswill@users.noreply.github.com>
2023-03-02 07:33:23 -0600

    Updates App.js for better elements distribution

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBarVertical } from "./Navigation/NavBarVertical.js";
 import { NavBarHorizontal } from "./Navigation/NavBarHorizontal.js";
 import { Outlet } from "react-router-dom";
-// import Authentication from "./Authentication/Authentication.js";
+import Authentication from "./Authentication/Authentication.js";
 
 export default function App() {
+
+
   return (
-    <div className="container-fluid App bg-dark">
-      {/*<Authentication />*/}
+    <div className="container-fluid App bg-dark row" style={{height:"100%", width:"100%"}}>
+      <Authentication />
       <NavBarHorizontal />
-      <NavBarVertical />
-      <Outlet />
+      <div className={"row"} style={{margin:"0", padding:"0"}}>
+        <NavBarVertical />
+        <Outlet />
+      </div>
     </div>
   );
 }
