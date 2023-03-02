a5f0fddea11435d8d21a452a66b77632854eca8f
gchriswill <4576678+gchriswill@users.noreply.github.com>
2023-03-02 07:29:18 -0600

    Updates vertical navigation

--- src/Navigation/NavBarVertical.js
+++ src/Navigation/NavBarVertical.js
@@ -3,18 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./NavBarVertical.css";
 import { NavBarVerticalLogo } from "./NavBarVerticalLogo";
 import { NavLink } from "react-router-dom";
+import React from "react";
 
 export function NavBarVertical() {
   return (
-  <div className="container-fluid row ShiaNavBarVerticalContainer" >
-      <div className="col-2">
-        <NavBarVerticalLogo/>
-        <Nav defaultActiveKey="/dashboard" className="flex-column ShiaNavBar">
-          <NavLink className="ShiaNavBarVerticalLinks" to={"/dashboard"}>Dashboard</NavLink>
-          <NavLink className="ShiaNavBarVerticalLinks" to={"/inventory"}>Inventory</NavLink>
-          <NavLink className="ShiaNavBarVerticalLinks" to={"/notifications"}>Notifications</NavLink>
-        </Nav>
-      </div>
+    <div className="ShiaNavBarVerticalContainer col-2">
+      <NavBarVerticalLogo />
+      <Nav defaultActiveKey="/dashboard" className="flex-column ShiaNavBar h-100" style={{ margin:"0", padding:"0"}}>
+        <NavLink className="ShiaNavBarVerticalLinks" to={"/dashboard"}>Dashboard</NavLink>
+        <NavLink className="ShiaNavBarVerticalLinks" to={"/inventory"}>Inventory</NavLink>
+        <NavLink className="ShiaNavBarVerticalLinks" to={"/notifications"}>Notifications</NavLink>
+      </Nav>
     </div>
   );
 }
