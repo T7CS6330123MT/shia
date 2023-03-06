5bd1ba79f1e9d4d6376c14801e8ff9e8ee33ca1d
gchriswill <4576678+gchriswill@users.noreply.github.com>
2023-03-06 04:57:31 -0600

    Adds new icons for Notifications

--- src/Notifications/Notifications.js
+++ src/Notifications/Notifications.js
@@ -1,35 +1,40 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
-// import DatastoreManager from "./DatastoreManager.js"
-// import DataParser from "./DataParser.js";
+import { ReactComponent as WarningIcon} from "../Assets/WarningIcon.svg";
+import { ReactComponent as OutstockIcon} from "../Assets/OutstockIcon.svg";
+// import DatabaseManager from "./DatabaseManager.js"
+// import InventoryParser from "./InventoryParser.js";
 
 
 export default function Inventory() {
 
-  // const dataParser = new DataParser()
-  // const datastoreManager = new DatastoreManager()
+  // const dataParser = new InventoryParser()
+  // const datastoreManager = new DatabaseManager()
 
   return (
     <>
       <Table striped bordered hover variant="dark" style={{width: "65%", margin: "0 auto auto 0", padding:"0"}}>
         <thead>
-        <tr style={{border:"none"}}><th style={{border:"inherit"}}><h1 style={{ display:"inline"}}>Notifications</h1></th></tr>
+        <tr style={{border:"none"}}>
+          <th style={{border:"inherit"}}><h1 style={{ display:"inline"}}>Notifications</h1></th>
+        </tr>
+        <tr>
+          <th className={"w-25"}>Type</th>
+          <th>Message</th>
+        </tr>
         </thead>
         <tbody>
         <tr key={0}>
-          <td><div>⚠️ Coca Cola low stock alert</div></td>
+          <td><WarningIcon style={{width:"32px", height:"32px", margin: "8px"}}/>️</td>
+          <td>Tide low stock alert</td>
         </tr>
         <tr key={1}>
-          <td><div>⚠️ Tide low stock alert</div></td>
+          <td><WarningIcon style={{width:"32px", height:"32px", margin: "8px"}}/>️</td>
+          <td>Pinesol low stock alert</td>
         </tr>
         <tr key={2}>
-          <td><div>⚠️ Pasta low stock alert</div></td>
-        </tr>
-        <tr key={3}>
-          <td><div>⚠️ Clorox low stock alert</div></td>
-        </tr>
-        <tr key={4}>
-          <td><div>❌️ Coca Cola out of stock alert</div></td>
+          <td><OutstockIcon style={{width:"32px", height:"32px", margin: "8px"}}/>️</td>
+          <td>Bread low out of stock alert</td>
         </tr>
         </tbody>
       </Table>
