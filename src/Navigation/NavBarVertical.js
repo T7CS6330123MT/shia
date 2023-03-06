72c1d0f36c377324dc365620120b44e187ca3bb6
gchriswill <4576678+gchriswill@users.noreply.github.com>
2023-03-06 04:56:47 -0600

    Adds new icons for the side navigation items

--- src/Navigation/NavBarVertical.js
+++ src/Navigation/NavBarVertical.js
@@ -4,15 +4,24 @@ import "./NavBarVertical.css";
 import { NavBarVerticalLogo } from "./NavBarVerticalLogo.js";
 import { NavLink } from "react-router-dom";
 import React from "react";
+import { ReactComponent as DashboardIcon} from "../Assets/DashboardIcon.svg";
+import { ReactComponent as SummaryIcon} from "../Assets/SummaryIcon.svg";
+import { ReactComponent as NotificationsIcon} from "../Assets/NotificationsIcon.svg";
 
 export default function NavBarVertical() {
   return (
     <div className="ShiaNavBarVerticalContainer col-2">
       <NavBarVerticalLogo />
-      <Nav defaultActiveKey="/dashboard" className="flex-column ShiaNavBar h-100">
-        <NavLink className="ShiaNavBarVerticalLinks" to={"/dashboard"}>Dashboard</NavLink>
-        <NavLink className="ShiaNavBarVerticalLinks" to={"/inventory"}>Inventory</NavLink>
-        <NavLink className="ShiaNavBarVerticalLinks" to={"/notifications"}>Notifications</NavLink>
+      <Nav defaultActiveKey="/dashboard" className="flex-column ShiaNavBar pt-5">
+        <NavLink className="ShiaNavBarVerticalLinks pb-3" to={"/dashboard"}>
+          <DashboardIcon style={{width:"24px", height:"24px", marginRight:"8px"}}/> Dashboard
+        </NavLink>
+        <NavLink className="ShiaNavBarVerticalLinks pb-3" to={"/inventory"}>
+          <SummaryIcon style={{width:"24px", height:"24px", marginRight:"8px"}}/> Inventory
+        </NavLink>
+        <NavLink className="ShiaNavBarVerticalLinks" to={"/notifications"}>
+          <NotificationsIcon style={{width:"24px", height:"24px", marginRight:"8px"}}/> Notifications
+        </NavLink>
       </Nav>
     </div>
   );
